6d525ba9ece9b9c00ff71e5d3acbbce7c67983a0
mgorkii <maksim.gorkii@nlplogix.com>
2021-10-20 14:14:03 -0400

    Configure terraform template

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -1,29 +1,185 @@
-variable "resource_group" {
-  description = "Name of the resource group, including the -rg"
-  default     = "udacity-project1-rg"
-  type        = string
+provider "azurerm" {
+  features {}
 }
 
-variable "packer_resource_group" {
-  description = "Name of the resource group where the packer image is"
-  default     =  "udacity-image-rg"
-  type        = string
+#create the resource group 
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
 }
 
-variable "prefix" {
-  description = "The prefix which should be used for all resources in the resource group specified"
-  default     = "udacity-pg"
-  type        = string
+#create a network and a subnet
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
-variable "num_of_vms" {
-  description = "Number of VM resources to create behund the load balancer"
-  default     = 3
-  type        = number
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-  default     = "Canada Central"
-  type        = string
+# create network security group. Allow access to other vm's 
+# on subnet and deny direct access from internet
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-netsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "VM"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = subnet.rg.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+# Create network interface
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  count = var.vm_number
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+#create a public IP 
+resource "azurerm_public_ip" "main" {
+  name                = "public-ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
+
+# create Load Balancer 
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIP"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_lb_probe" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "http-server-probe"
+  port                = 8080
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "${var.resource_group_name_prefix}-lb-backend-pool"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "main" {
+  count                   = var.num_of_vms
+  network_interface_id    = azurerm_network_interface.main[count.index].id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
+}
+
+
+resource "azurerm_lb_rule" "main" {
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = "HTTP"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 8080
+  frontend_ip_configuration_name = azurerm_lb.main.frontend_ip_configuration[0].name
+  probe_id                       = azurerm_lb_probe.main.id
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.main.id
+}
+
+data "azurerm_resource_group" "image" {
+  name                = var.packer_resource_group
+}
+
+data "azurerm_image" "image" {
+  name                = var.packer_image_name
+  resource_group_name = data.azurerm_resource_group.image.name
+}
+
+resource "azurerm_availability_set" "main" {
+  name                        = "${var.resource_group_name_prefix}-aset"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  platform_fault_domain_count = 2
+}
+
+#Create virtual machines using packer image
+resource "azurerm_linux_virtual_machine" "example" {
+  count                           = var.vm_number
+  name                            = "${var.prefix}-${count.index}-vm"
+  resource_group_name             = azurerm_resource_group.example.name
+  location                        = azurerm_resource_group.example.location
+  size                            = "Standard_B1ls"
+  admin_username                  = var.admin_user
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index)]
+  availability_set_id = azurerm_availability_set.main.id
+
+  storage_profile_image_reference {
+    id=data.azurerm_image.image.id
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  tags = {
+    environment = "dev",
+    project-name  = "Deploying a Web Server in Azure"
+
+  }
+}
+
+#create vurtual disks
+resource "azurerm_managed_disk" "main" {
+  count                           = var.vm_number
+  name                            = "${var.prefix}-${count.index}-mdisk"
+  location                        = azurerm_resource_group.main.location
+  resource_group_name             = azurerm_resource_group.main.name
+  storage_account_type            = "Standard_LRS"
+  create_option                   = "Empty"
+  disk_size_gb                    = 1
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "main" {
+  count              = var.vm_number
+  managed_disk_id    = azurerm_managed_disk.main.*.id[count.index]
+  virtual_machine_id = azurerm_linux_virtual_machine.main.*.id[count.index]
+  lun                = 10 * count.index
+  caching            = "ReadWrite"
+}
+
+output "lb_url" {
+  value       = "http://${azurerm_public_ip.main.ip_address}/"
+  description = "This is the URL for the LB."
 }
